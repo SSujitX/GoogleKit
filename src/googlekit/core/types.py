5b31d7095c812e4ba4ae96847edab49acebf1cec
SSujitX <ssujitxx@gmail.com>
2026-07-11 23:57:04 +0600

    feat: Add core module file types.py

---
+++ src/googlekit/core/types.py
@@ -0,0 +1,16 @@
+"""Common type aliases."""
+
+from __future__ import annotations
+
+from typing import TypeAlias
+
+FileId: TypeAlias = str
+FolderId: TypeAlias = str
+SpreadsheetId: TypeAlias = str
+DocumentId: TypeAlias = str
+PresentationId: TypeAlias = str
+CalendarId: TypeAlias = str
+EventId: TypeAlias = str
+PermissionId: TypeAlias = str
+Scope: TypeAlias = str
+A1Range: TypeAlias = str
